cac62b75c5d1e0e49cd723c531f02f50334391d0
Marcos Bertucci <m.vitti.bertucci@hotmail.com>
2025-02-18 21:48:42 -0300

    Criar função Adicionar e Atualizar Amigos

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
 // Criando um arry para armazenar a lista de amigos
-let amgigos = [];
+let amigos = [];
+
+// Criando a função para adicionar amigos
+function adicionarAmigo() {
+    let inserirNome = document.querySelector('input');
+    if (inserirNome.value === "") {
+        alert('Por favor, insira um nome!');
+    } else {
+        amigos.push(inserirNome.value);
+    };
+    inserirNome.value = "";
+    atualizarListaAmigos()
+};
+
+// Percorrendo o array amigos
+function atualizarListaAmigos() {
+       let lista = document.getElementById('listaAmigos');
+       lista.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = amigos[i]
+        lista.appendChild(li)
+    }
+}
